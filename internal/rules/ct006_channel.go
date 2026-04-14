7ddbb9482b690226be1fe4641e631ac05ae0b9da
oasilturk <moasilturk@gmail.com>
2026-04-14 16:23:36 +0300

    refactor: Extract channelSecretParam helper in CT006, remove dead code in taint

--- internal/rules/ct006_channel.go
+++ internal/rules/ct006_channel.go
@@ -14,6 +14,14 @@ import (
 	"github.com/oasilturk/ctguard/internal/taint"
 )
 
+// channelSecretParam checks if a channel value is a secret parameter.
+func channelSecretParam(ch ssa.Value, secretParams map[string]bool) (string, confidence.ConfidenceLevel, bool) {
+	if p, ok := ch.(*ssa.Parameter); ok && secretParams[p.Name()] {
+		return p.Name(), confidence.ConfidenceHigh, true
+	}
+	return "", confidence.ConfidenceLow, false
+}
+
 // CT006 flags secrets that are sent to or received from channels.
 func RunCT006(pass *analysis.Pass, ssaRes *buildssa.SSA, secrets annotations.Secrets, ipAnalyzer *taint.InterproceduralAnalyzer) FindingList {
 	var findings FindingList
@@ -33,13 +41,7 @@ func RunCT006(pass *analysis.Pass, ssaRes *buildssa.SSA, secrets annotations.Sec
 				if send, ok := ins.(*ssa.Send); ok {
 					secretName, conf := dep.DependsOn(send.X)
 					if secretName == "" {
-						// Check if the channel itself is a secret parameter
-						if p, ok := send.Chan.(*ssa.Parameter); ok {
-							if secretParams[p.Name()] {
-								secretName = p.Name()
-								conf = confidence.ConfidenceHigh
-							}
-						}
+						secretName, conf, _ = channelSecretParam(send.Chan, secretParams)
 					}
 					if secretName == "" {
 						continue
@@ -65,14 +67,8 @@ func RunCT006(pass *analysis.Pass, ssaRes *buildssa.SSA, secrets annotations.Sec
 				// channel receive is: ssa.UnOp with Op = token.ARROW
 				if unop, ok := ins.(*ssa.UnOp); ok && unop.Op == token.ARROW {
 					secretName, conf := dep.IsTaintedChannel(unop.X)
-
 					if secretName == "" {
-						if p, ok := unop.X.(*ssa.Parameter); ok {
-							if secretParams[p.Name()] {
-								secretName = p.Name()
-								conf = confidence.ConfidenceHigh
-							}
-						}
+						secretName, conf, _ = channelSecretParam(unop.X, secretParams)
 					}
 
 					if secretName == "" {
@@ -104,12 +100,7 @@ func RunCT006(pass *analysis.Pass, ssaRes *buildssa.SSA, secrets annotations.Sec
 							if st.Send != nil {
 								secretName, conf := dep.DependsOn(st.Send)
 								if secretName == "" {
-									if p, ok := st.Chan.(*ssa.Parameter); ok {
-										if secretParams[p.Name()] {
-											secretName = p.Name()
-											conf = confidence.ConfidenceHigh
-										}
-									}
+									secretName, conf, _ = channelSecretParam(st.Chan, secretParams)
 								}
 								if secretName != "" {
 									pos := bestPos(st.Pos, fn.Pos())
@@ -131,12 +122,7 @@ func RunCT006(pass *analysis.Pass, ssaRes *buildssa.SSA, secrets annotations.Sec
 							// Receive case: <- st.Chan
 							secretName, conf := dep.IsTaintedChannel(st.Chan)
 							if secretName == "" {
-								if p, ok := st.Chan.(*ssa.Parameter); ok {
-									if secretParams[p.Name()] {
-										secretName = p.Name()
-										conf = confidence.ConfidenceHigh
-									}
-								}
+								secretName, conf, _ = channelSecretParam(st.Chan, secretParams)
 							}
 							if secretName != "" {
 								pos := bestPos(st.Pos, fn.Pos())
